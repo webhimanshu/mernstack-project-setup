2584c75d5516075b1cbea90ee1fc34e95e91e769
webhimanshu <himanshusharma2709@gmail.com>
2024-03-07 17:29:37 +0530

    video model

---
+++ backend/src/models/video.model.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const mongooseAggregatePaginate = required('mongoose-aggregate-paginate-v2');
+const videoSchema = mongoose.Schema({
+
+    videoFile: {
+        type: String,
+        required: true,
+    },
+
+    thumbnail: {
+        type: String,
+        required: true,
+
+    },
+
+    title: {
+        type: String,
+        required: true,
+    },
+
+    description: {
+        type: String,
+        required: true,
+    },
+
+    duration: {
+        type: Number,
+        required: true,
+    },
+
+    views: {
+        type: Number,
+        default: 0
+    },
+
+    isPublished: {
+        type: Boolean,
+        default: true
+    },
+
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+
+}, { timestamps: true });   
+
+videoSchema.plugin(mongooseAggregatePaginate);
+const Video = mongoose.model('Video', videoSchema);
+module.exports = Video;
