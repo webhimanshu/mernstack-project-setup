64a0b1d1fd4444314c4c4573604ea85cbfd8ca8f
webhimanshu <himanshusharma2709@gmail.com>
2024-03-07 17:42:41 +0530

    custom method written for generate access token and refresh token

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -55,14 +55,33 @@ const userSchema = mongoose.Schema({
     },
 }, { timestamps: true });
 
-userSchema.pre("save", async function(next){ // this will hash password before saving in db
-    if(!this.isModified("password")) return next();
+userSchema.pre("save", async function (next) { // this will hash password before saving in db
+    if (!this.isModified("password")) return next();
     this.password = bcrypt.hash(this.password, 10);
 });
 
-userSchema.methods.isPasswordCorrect = async function(password){ // this will create custom method and compare password
-   return await bcrypt.compare(password, this.password);
+userSchema.methods.isPasswordCorrect = async function (password) { // this will create custom method and compare password and it will inject methods in schema
+    return await bcrypt.compare(password, this.password);
 };
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        username: this.username,
+        fullName: this.fullName,
+        email: this.email
+    }, process.env.ACCESS_TOKEN_SECRET, {
+        expiredIn: ACCESS_TOKEN_EXPIRY
+    });
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        _id: this._id,
+    }, process.env.REFRESH_TOKEN_SECRET, {
+        expiredIn: REFRESH_TOKEN_EXPIRY
+    });
+}
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
